24988fe9507af195736a0895823144cd40d803b3
nelo360 <nelo360@gmail.com>
2019-01-07 23:21:53 +0100

    add missing import

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -36,6 +36,7 @@ import android.os.Vibrator;
 import android.provider.Settings;
 import android.service.quicksettings.Tile;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
