Havoc-OS/android_frameworks_base

base: qs status bar header image [1/4]
Change-Id: Ie295f7f35208545dc3146b1fdc7ad697c5d70e16

core/java/com/android/internal/util/havoc/PackageUtils.java
@@ -1,0 +1,46 @@
+/*
+* Copyright (C) 2014-2018 The OmniROM Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.internal.util.havoc;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+public class PackageUtils {
+
+    public static boolean isAppInstalled(Context context, String appUri) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            pm.getPackageInfo(appUri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isAvailableApp(String packageName, Context context) {
+        Context mContext = context;
+        final PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/havoc/FileHeaderProvider.java
@@ -1,0 +1,126 @@
+/*
+ *  Copyright (C) 2018 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.systemui.havoc;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.util.Log;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Calendar;
+
+import com.android.systemui.R;
+
+public class FileHeaderProvider implements
+        StatusBarHeaderMachine.IStatusBarHeaderProvider {
+
+    public static final String TAG = "FileHeaderProvider";
+    private static final boolean DEBUG = false;
+    private static final String HEADER_FILE_NAME = "custom_file_header_image";
+
+    private Context mContext;
+    private Drawable mImage;
+
+    public FileHeaderProvider(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public String getName() {
+        return "file";
+    }
+
+    @Override
+    public void settingsChanged(Uri uri) {
+        final boolean customHeader = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0,
+                UserHandle.USER_CURRENT) == 1;
+
+        if (uri != null && uri.equals(Settings.System.getUriFor(
+                Settings.System.STATUS_BAR_FILE_HEADER_IMAGE))) {
+            String imageUri = Settings.System.getStringForUser(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_FILE_HEADER_IMAGE,
+                    UserHandle.USER_CURRENT);
+            if (imageUri != null) {
+                saveHeaderImage(Uri.parse(imageUri));
+            }
+        }
+        if (customHeader) {
+            loadHeaderImage();
+        }
+    }
+
+    @Override
+    public void enableProvider() {
+        settingsChanged(null);
+    }
+
+    @Override
+    public void disableProvider() {
+    }
+
+    private void saveHeaderImage(Uri imageUri) {
+        if (DEBUG) Log.i(TAG, "Save header image " + " " + imageUri);
+        try {
+            final InputStream imageStream = mContext.getContentResolver().openInputStream(imageUri);
+            File file = new File(mContext.getFilesDir(), HEADER_FILE_NAME);
+            if (file.exists()) {
+                file.delete();
+            }
+            FileOutputStream output = new FileOutputStream(file);
+            byte[] buffer = new byte[8 * 1024];
+            int read;
+
+            while ((read = imageStream.read(buffer)) != -1) {
+                output.write(buffer, 0, read);
+            }
+            output.flush();
+            if (DEBUG) Log.i(TAG, "Saved header image " + " " + file.getAbsolutePath());
+        } catch (IOException e) {
+            Log.e(TAG, "Save header image failed " + " " + imageUri);
+        }
+    }
+
+    private void loadHeaderImage() {
+        mImage = null;
+        File file = new File(mContext.getFilesDir(), HEADER_FILE_NAME);
+        if (file.exists()) {
+            if (DEBUG) Log.i(TAG, "Load header image");
+            final Bitmap image = BitmapFactory.decodeFile(file.getAbsolutePath());
+            mImage = new BitmapDrawable(mContext.getResources(), image);
+        }
+    }
+
+    @Override
+    public Drawable getCurrent(final Calendar now) {
+        return mImage;
+    }
+}

packages/SystemUI/src/com/android/systemui/havoc/StaticHeaderProvider.java
@@ -1,0 +1,117 @@
+/*
+ *  Copyright (C) 2017 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.systemui.havoc;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Calendar;
+
+import com.android.systemui.R;
+import com.android.internal.util.havoc.PackageUtils;
+
+public class StaticHeaderProvider implements
+        StatusBarHeaderMachine.IStatusBarHeaderProvider {
+
+    public static final String TAG = "StaticHeaderProvider";
+    private static final boolean DEBUG = false;
+
+    private Context mContext;
+    private Resources mRes;
+    private String mImage;
+    private String mPackageName;
+
+    public StaticHeaderProvider(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public String getName() {
+        return "static";
+    }
+
+    @Override
+    public void settingsChanged(Uri uri) {
+        final boolean customHeader = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0,
+                UserHandle.USER_CURRENT) == 1;
+        String imageUrl = Settings.System.getStringForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER_IMAGE,
+                UserHandle.USER_CURRENT);
+
+        if (imageUrl != null && customHeader) {
+            int idx = imageUrl.indexOf("/");
+            if (idx != -1) {
+                String[] parts = imageUrl.split("/");
+                mPackageName = parts[0];
+                mImage = parts[1];
+                loadHeaderImage();
+            }
+        }
+    }
+
+    @Override
+    public void enableProvider() {
+        settingsChanged(null);
+    }
+
+    @Override
+    public void disableProvider() {
+    }
+
+    private void loadHeaderImage() {
+        if (DEBUG) Log.i(TAG, "Load header image " + " " + mPackageName + " " + mImage);
+
+        try {
+            PackageManager packageManager = mContext.getPackageManager();
+            mRes = packageManager.getResourcesForApplication(mPackageName);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to load icon pack " + mPackageName, e);
+            mRes = null;
+        }
+    }
+
+    @Override
+    public Drawable getCurrent(final Calendar now) {
+        if (mRes == null) {
+            return null;
+        }
+        if (!PackageUtils.isAvailableApp(mPackageName, mContext)) {
+            Log.w(TAG, "Header pack image " + mImage + " no longer available");
+            return null;
+        }
+        try {
+            return mRes.getDrawable(mRes.getIdentifier(mImage, "drawable", mPackageName), null);
+        } catch(Resources.NotFoundException e) {
+            Log.w(TAG, "No drawable found for " + now +" in " + mPackageName);
+        }
+        return null;
+    }
+}

MagneticOS-P/frameworks_base

core/java/com/android/internal/util/custom/PackageUtils.java
@@ -1,0 +1,46 @@
+/*
+* Copyright (C) 2014-2018 The OmniROM Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.internal.util.custom;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+public class PackageUtils {
+
+    public static boolean isAppInstalled(Context context, String appUri) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            pm.getPackageInfo(appUri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isAvailableApp(String packageName, Context context) {
+        Context mContext = context;
+        final PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/revo/FileHeaderProvider.java
@@ -1,0 +1,126 @@
+/*
+ *  Copyright (C) 2018 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.systemui.revo;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.util.Log;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Calendar;
+
+import com.android.systemui.R;
+
+public class FileHeaderProvider implements
+        StatusBarHeaderMachine.IStatusBarHeaderProvider {
+
+    public static final String TAG = "FileHeaderProvider";
+    private static final boolean DEBUG = false;
+    private static final String HEADER_FILE_NAME = "custom_file_header_image";
+
+    private Context mContext;
+    private Drawable mImage;
+
+    public FileHeaderProvider(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public String getName() {
+        return "file";
+    }
+
+    @Override
+    public void settingsChanged(Uri uri) {
+        final boolean customHeader = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0,
+                UserHandle.USER_CURRENT) == 1;
+
+        if (uri != null && uri.equals(Settings.System.getUriFor(
+                Settings.System.STATUS_BAR_FILE_HEADER_IMAGE))) {
+            String imageUri = Settings.System.getStringForUser(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_FILE_HEADER_IMAGE,
+                    UserHandle.USER_CURRENT);
+            if (imageUri != null) {
+                saveHeaderImage(Uri.parse(imageUri));
+            }
+        }
+        if (customHeader) {
+            loadHeaderImage();
+        }
+    }
+
+    @Override
+    public void enableProvider() {
+        settingsChanged(null);
+    }
+
+    @Override
+    public void disableProvider() {
+    }
+
+    private void saveHeaderImage(Uri imageUri) {
+        if (DEBUG) Log.i(TAG, "Save header image " + " " + imageUri);
+        try {
+            final InputStream imageStream = mContext.getContentResolver().openInputStream(imageUri);
+            File file = new File(mContext.getFilesDir(), HEADER_FILE_NAME);
+            if (file.exists()) {
+                file.delete();
+            }
+            FileOutputStream output = new FileOutputStream(file);
+            byte[] buffer = new byte[8 * 1024];
+            int read;
+
+            while ((read = imageStream.read(buffer)) != -1) {
+                output.write(buffer, 0, read);
+            }
+            output.flush();
+            if (DEBUG) Log.i(TAG, "Saved header image " + " " + file.getAbsolutePath());
+        } catch (IOException e) {
+            Log.e(TAG, "Save header image failed " + " " + imageUri);
+        }
+    }
+
+    private void loadHeaderImage() {
+        mImage = null;
+        File file = new File(mContext.getFilesDir(), HEADER_FILE_NAME);
+        if (file.exists()) {
+            if (DEBUG) Log.i(TAG, "Load header image");
+            final Bitmap image = BitmapFactory.decodeFile(file.getAbsolutePath());
+            mImage = new BitmapDrawable(mContext.getResources(), image);
+        }
+    }
+
+    @Override
+    public Drawable getCurrent(final Calendar now) {
+        return mImage;
+    }
+}

packages/SystemUI/src/com/android/systemui/revo/StaticHeaderProvider.java
@@ -1,0 +1,117 @@
+/*
+ *  Copyright (C) 2017 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.systemui.revo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Calendar;
+
+import com.android.systemui.R;
+import com.android.internal.util.custom.PackageUtils;
+
+public class StaticHeaderProvider implements
+        StatusBarHeaderMachine.IStatusBarHeaderProvider {
+
+    public static final String TAG = "StaticHeaderProvider";
+    private static final boolean DEBUG = false;
+
+    private Context mContext;
+    private Resources mRes;
+    private String mImage;
+    private String mPackageName;
+
+    public StaticHeaderProvider(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public String getName() {
+        return "static";
+    }
+
+    @Override
+    public void settingsChanged(Uri uri) {
+        final boolean customHeader = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0,
+                UserHandle.USER_CURRENT) == 1;
+        String imageUrl = Settings.System.getStringForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER_IMAGE,
+                UserHandle.USER_CURRENT);
+
+        if (imageUrl != null && customHeader) {
+            int idx = imageUrl.indexOf("/");
+            if (idx != -1) {
+                String[] parts = imageUrl.split("/");
+                mPackageName = parts[0];
+                mImage = parts[1];
+                loadHeaderImage();
+            }
+        }
+    }
+
+    @Override
+    public void enableProvider() {
+        settingsChanged(null);
+    }
+
+    @Override
+    public void disableProvider() {
+    }
+
+    private void loadHeaderImage() {
+        if (DEBUG) Log.i(TAG, "Load header image " + " " + mPackageName + " " + mImage);
+
+        try {
+            PackageManager packageManager = mContext.getPackageManager();
+            mRes = packageManager.getResourcesForApplication(mPackageName);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to load icon pack " + mPackageName, e);
+            mRes = null;
+        }
+    }
+
+    @Override
+    public Drawable getCurrent(final Calendar now) {
+        if (mRes == null) {
+            return null;
+        }
+        if (!PackageUtils.isAvailableApp(mPackageName, mContext)) {
+            Log.w(TAG, "Header pack image " + mImage + " no longer available");
+            return null;
+        }
+        try {
+            return mRes.getDrawable(mRes.getIdentifier(mImage, "drawable", mPackageName), null);
+        } catch(Resources.NotFoundException e) {
+            Log.w(TAG, "No drawable found for " + now +" in " + mPackageName);
+        }
+        return null;
+    }
+}